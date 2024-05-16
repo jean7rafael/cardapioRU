93f2a5eaa0f1ee7ea5b0b5b246dcd7d923e78c46
jean7rafael <jean7rafael@ufpr.br>
2024-05-16 00:47:47 -0300

    firebase config

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def obter_cardapio():
             data_atual = datetime.now().strftime('%d/%m/%y')
             hora_atual = datetime.now().time()
 
-            dias = cardapio_div.find_all('p', string=lambda x: x and data_atual in x)
+            dias = cardapio_div.find_all('p', string=lambda x: x and any(day in x for day in ['Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo', 'Segunda-feira', 'Terça-feira']))
             if not dias:
                 return jsonify(message="Não foi possível encontrar as datas.")
 
@@ -29,19 +29,41 @@ def obter_cardapio():
             if not figuras:
                 return jsonify(message="Não foi possível encontrar figuras com a classe 'wp-block-table'.")
 
+            # Função para obter a primeira linha não vazia após um índice dado
+            def get_next_non_empty_line(lines, start_index):
+                for i in range(start_index + 1, len(lines)):
+                    if lines[i].strip():
+                        return lines[i]
+                return "Não foi possível encontrar a informação desejada."
+
+            # Processar blocos de datas
+            cardapio_blocos = []
             for dia, fig in zip(dias, figuras):
                 dia_texto = dia.get_text()
-                if data_atual in dia_texto:
-                    lines = fig.get_text(separator='\n').split('\n')
-                    almoco_index = lines.index('ALMOÇO')
-                    jantar_index = lines.index('JANTAR')
-                    almoco_linha = lines[almoco_index + 1]
-                    jantar_linha = lines[jantar_index + 1]
-                    
+                lines = fig.get_text(separator='\n').split('\n')
+                almoco_index = lines.index('ALMOÇO')
+                jantar_index = lines.index('JANTAR')
+
+                almoco_linha = get_next_non_empty_line(lines, almoco_index)
+                jantar_linha = get_next_non_empty_line(lines, jantar_index)
+                
+                bloco = {
+                    'data': dia_texto,
+                    'almoco': almoco_linha,
+                    'jantar': jantar_linha
+                }
+                cardapio_blocos.append(bloco)
+
+            # Descomente a linha abaixo se quiser retornar todos os blocos encontrados
+            # return jsonify(cardapio_blocos=cardapio_blocos)
+            
+            # Lógica atual para retornar refeição desejada com base no horário
+            for bloco in cardapio_blocos:
+                if data_atual in bloco['data']:
                     if hora_atual < datetime.strptime('14:00', '%H:%M').time():
-                        refeicao_desejada = f"Hoje para almoçar tem {almoco_linha}"
+                        refeicao_desejada = f"Hoje para almoçar tem {bloco['almoco']}"
                     else:
-                        refeicao_desejada = f"Hoje para jantar tem {jantar_linha}"
+                        refeicao_desejada = f"Hoje para jantar tem {bloco['jantar']}"
                     return jsonify(message=refeicao_desejada)
 
             return jsonify(message="Não foi possível encontrar o cardápio para hoje.")
--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,19 @@
-const functions = require("firebase-functions");
-const {Nuxt} = require("nuxt-start");
-const express = require("express");
-const {spawn} = require("child_process");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-const app = express();
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-// Configurar Nuxt.js
-const isDev = process.env.NODE_ENV !== "production";
-const nuxtConfig = require("./nuxt.config.js");
-nuxtConfig.dev = isDev;
-const nuxt = new Nuxt(nuxtConfig);
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-// Middleware para Nuxt.js
-app.use(nuxt.render);
-
-// Rota para o Flask
-app.get("/cardapio", (req, res) => {
-  const process = spawn("python3", ["main.py"]);
-  process.stdout.on("data", (data) => {
-    res.send(data.toString());
-  });
-  process.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
-    res.status(500).send(data.toString());
-  });
-});
-
-exports.api = functions.https.onRequest(app);
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
